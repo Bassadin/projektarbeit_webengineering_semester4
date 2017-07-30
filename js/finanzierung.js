9bd29bc9d6f82f0396fd1804c13ff8735080b60b
Bastian Hodapp <bastianhodapp@gmail.com>
2017-07-30 15:45:17 +0200

    Further progressed calculation script

--- js/finanzierung.js
+++ js/finanzierung.js
@@ -1,8 +1,32 @@
 $(document).ready(function() {
-    updateSumOfAdditonalBuildingCosts();
+    updateAllCosts();
+    document.getElementById("buildingCost").oninput = function() {
+        updateAllCosts();
+    };
+    document.getElementById("ownCapital").oninput = function() {
+        updateAllCosts();
+    };
+    document.getElementById("estateCost").oninput = function() {
+        updateAllCosts();
+    };
 });
 
-function updateSumOfAdditonalBuildingCosts() {
-    // console.log("baum");
-    $("#agentsFee").text($("#buildingCost").text * 0.047);
-};
+function updateAllCosts() {
+    let estateCostValue = parseInt($("#estateCost").val() + 1 * 0); //+ 1 * 0 -> hack for empty field, removes possibility of NaN output
+
+    let agentsFee = estateCostValue * 0.047;
+    let notaryFee = estateCostValue * 0.02;
+    let realEstateTransferTax = estateCostValue * 0.05;
+    let sumOfAllAdditionalBuildingCosts = agentsFee + notaryFee + realEstateTransferTax;
+
+    $("#agentsFee").text(agentsFee.toFixed(2));
+    $("#notaryFee").text(notaryFee.toFixed(2));
+    $("#realEstateTransferTax").text(realEstateTransferTax.toFixed(2));
+
+    $("#sumOfAdditonalBuildingCosts").text(sumOfAllAdditionalBuildingCosts.toFixed(2));
+
+    $("#loanAmount").text((parseInt($("#buildingCost").val() + 1 * 0) +
+        estateCostValue +
+        sumOfAllAdditionalBuildingCosts -
+        parseInt($("#ownCapital").val() + 1 * 0)).toFixed(2));
+}
