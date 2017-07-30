454503133725393607372034de0ff56abaed0cb7
Bastian Hodapp <bastianhodapp@gmail.com>
2017-07-30 16:30:04 +0200

    Finished Basic cost calculation

--- js/finanzierung.js
+++ js/finanzierung.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
 });
 
 function updateAllCosts() {
-    let estateCostValue = parseInt($("#estateCost").val() + 1 * 0); //+ 1 * 0 -> hack for empty field, removes possibility of NaN output
+    let estateCostValue = parseInt($("#estateCost").val()) + 1 * 0; //+ 1 * 0 -> hack for empty field, removes possibility of NaN output
 
     let agentsFee = estateCostValue * 0.047;
     let notaryFee = estateCostValue * 0.02;
@@ -25,8 +25,34 @@ function updateAllCosts() {
 
     $("#sumOfAdditonalBuildingCosts").text(sumOfAllAdditionalBuildingCosts.toFixed(2));
 
-    $("#loanAmount").text((parseInt($("#buildingCost").val() + 1 * 0) +
+    let ownCapital = parseInt($("#ownCapital").val()) + 1 * 0;
+    let loanAmount = parseInt($("#buildingCost").val()) + 1 * 0 +
         estateCostValue +
         sumOfAllAdditionalBuildingCosts -
-        parseInt($("#ownCapital").val() + 1 * 0)).toFixed(2));
+        ownCapital;
+
+    let loanAmountText = "";
+
+    if (loanAmount <= 50000000 && loanAmount >= 50000) {
+        if (ownCapital >= loanAmount * 0.2) {
+            loanAmountText = loanAmount.toFixed(2) + "€";
+        } else {
+            loanAmountText = "FEHLER: Ihr Eigenkapital reicht nicht aus, um ein Darlehen anzunehmen";
+        }
+    } else {
+        if (loanAmount > 5000000) {
+            loanAmountText = "FEHLER: Wir vergeben keine Darlehen über 5.000.000€.";
+        } else {
+            loanAmountText = "FEHLER: Wir vergeben keine Darlehen unter 50.000€.";
+        }
+        loanAmountText = loanAmountText + " Mit den aktuellen Werten beträgt dieser " + loanAmount + "€."
+    }
+
+
+    $("#loanAmount").text(loanAmountText);
+    calculateRepaymentData(loanAmount);
+}
+
+function calculateRepaymentData(loanAmount) {
+
 }
